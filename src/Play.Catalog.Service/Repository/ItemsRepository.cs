da30663bf3f3a80551c45ae7cf1efe30ad955e2d
polyteh <prevoivan@gmail.com>
2025-03-23 21:18:07 +0200

    add repository

---
+++ src/Play.Catalog.Service/Repository/ItemsRepository.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Play.Catalog.Service.Repository.Models;
+
+namespace Play.Catalog.Service.Repository
+{
+    public class ItemsRepository
+    {
+        private const string collectionName="items";
+        private readonly IMongoCollection<Item> dbCollection;
+        private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
+        public ItemsRepository()
+        {
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var databse = mongoClient.GetDatabase("Catalog");
+            dbCollection = databse.GetCollection<Item>(collectionName);
+        }
+
+        public async Task<IReadOnlyCollection<Item>> GetAllAsync(){
+            return await dbCollection.Find(filterBuilder.Empty).ToListAsync();
+        }
+
+        public async Task<Item> GetAsync(Guid id){
+            FilterDefinition<Item> filter = filterBuilder.Eq(ent=>ent.Id, id );
+
+            return await dbCollection.Find(filter).FirstOrDefaultAsync();
+        }
+
+        public async Task CreateAsync(Item entity){
+            if(entity == null){
+                throw new ArgumentException(nameof(entity));
+            }
+
+            await dbCollection.InsertOneAsync(entity);
+        }
+
+        public async Task UpdateAsync(Item entity){
+            if(entity == null){
+                throw new ArgumentException(nameof(entity));
+            }
+
+            FilterDefinition<Item> filter = filterBuilder.Eq(existingItem=>existingItem.Id, entity.Id );
+
+            await dbCollection.ReplaceOneAsync(filter, entity);
+        }
+
+        public async Task RemoveAsync(Guid id){
+             FilterDefinition<Item> filter = filterBuilder.Eq(ent=>ent.Id, id );
+             await dbCollection.DeleteOneAsync(filter);
+        }
+    }
+}
--- src/Play.Catalog.Service/Repository/Models/Item.cs
+++ src/Play.Catalog.Service/Repository/Models/Item.cs
@@ -4,7 +4,7 @@ namespace Play.Catalog.Service.Repository.Models
 {
     public class Item
     {
-        public Guid IdMyProperty { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
